4762656ba99eb1cfb6cae9b595995faa994dbfc1
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-02-27 08:07:06 +0100

    Setter opp secrets

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM navikt/java:11
 
 COPY build/libs/app.jar ./
+RUN chmod +x scripts/run.sh
+COPY scripts /init-scripts
---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -eu
+
+if test -f /var/run/secrets/nais.io/serviceuser/username
+then
+    export SYSTEMBRUKER_USERNAME="$(cat /var/run/secrets/nais.io/serviceuser/username)"
+    export SYSTEMBRUKER_PASSWORD="$(cat /var/run/secrets/nais.io/serviceuser/password)"
+fi
