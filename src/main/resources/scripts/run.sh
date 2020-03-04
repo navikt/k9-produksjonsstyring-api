8c129c9b55e466efe0780d3ec8ccfdefa8b6e56b
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-03-04 09:33:53 +0100

    Kafka passord

--- src/main/resources/scripts/run.sh
+++ src/main/resources/scripts/run.sh
@@ -3,6 +3,6 @@ set -eu
 
 if test -f /var/run/secrets/nais.io/serviceuser/username
 then
-    export SERVICE_ACCOUNT_CLIENT_ID="$(cat /var/run/secrets/nais.io/serviceuser/username)"
-    export SERVICE_ACCOUNT_CLIENT_SECRET="$(cat /var/run/secrets/nais.io/serviceuser/password)"
+    export SYSTEMBRUKER_USERNAME="$(cat /var/run/secrets/nais.io/serviceuser/username)"
+    export SYSTEMBRUKER_PASSWORD="$(cat /var/run/secrets/nais.io/serviceuser/password)"
 fi
