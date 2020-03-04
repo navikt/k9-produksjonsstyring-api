36d1eb6d9fcabec5c8118dc2c2e27b8a7d14826c
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-03-04 14:10:24 +0100

    Clean database mens vi ikke er i prod

--- src/main/kotlin/no/nav/k9/db/Postgres.kt
+++ src/main/kotlin/no/nav/k9/db/Postgres.kt
@@ -42,6 +42,14 @@ fun Application.migrate(configuration: Configuration) =
     }
 
 fun runMigration(dataSource: DataSource, initSql: String? = null): Int {
+    // todo: fjernes når vi går i prod 
+    Flyway.configure()
+        .locations("migreringer/")
+        .dataSource(dataSource)
+        .initSql(initSql)
+        .load()
+        .clean()
+
     return Flyway.configure()
         .locations("migreringer/")
         .dataSource(dataSource)
