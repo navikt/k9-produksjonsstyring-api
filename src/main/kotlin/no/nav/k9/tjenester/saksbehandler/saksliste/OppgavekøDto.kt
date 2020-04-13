a956c866e112edebe4058cff59503353430fd5cf
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-04-13 08:46:37 +0200

    Oppdatert api

--- src/main/kotlin/no/nav/k9/tjenester/saksbehandler/saksliste/OppgavekøDto.kt
+++ src/main/kotlin/no/nav/k9/tjenester/saksbehandler/saksliste/OppgavekøDto.kt
@@ -1,18 +1,21 @@
 package no.nav.k9.tjenester.saksbehandler.saksliste
 
 import no.nav.k9.domene.lager.oppgave.KøSortering
-import no.nav.k9.domene.modell.*
+import no.nav.k9.domene.modell.BehandlingType
+import no.nav.k9.domene.modell.FagsakYtelseType
+import no.nav.k9.domene.modell.OppgaveKø
 import java.time.LocalDate
 import java.util.*
 
-class SakslisteDto(o: OppgaveKø, antallBehandlinger: Int) {
-    val sakslisteId: Int = Random().nextInt(56)
+class OppgavekøDto(o: OppgaveKø, antallBehandlinger: Int) {
+    val sakslisteId: UUID = o.id
     val navn: String
     var behandlingTyper: List<BehandlingType> = o.filtreringBehandlingTyper
     val sistEndret: LocalDate
-    var sortering: SorteringDto = SorteringDto(KøSortering.OPPRETT_BEHANDLING, 8976, 97, LocalDate.now(), LocalDate.now(), true)
+    var sortering: SorteringDto =
+        SorteringDto(KøSortering.OPPRETT_BEHANDLING, 8976, 97, LocalDate.now(), LocalDate.now(), true)
     var fagsakYtelseTyper: List<FagsakYtelseType> = o.filtreringYtelseTyper
-    var andreKriterier: List<AndreKriterierDto>  = emptyList()
+    var andreKriterier: List<AndreKriterierDto> = emptyList()
     var antallBehandlinger = 78
 
 
