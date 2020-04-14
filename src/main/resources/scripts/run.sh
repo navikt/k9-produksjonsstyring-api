0a7eb408d171fbbd2f0fa97a04907e2bed13b4bb
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-04-14 12:29:03 +0200

    Pdl oppdatering

--- src/main/resources/scripts/run.sh
+++ src/main/resources/scripts/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
-
+echo "Setting SYSTEMBRUKER_USERNAME and SYSTEMBRUKER_PASSWORD"
 if test -f /var/run/secrets/nais.io/serviceuser/username
 then
     export SYSTEMBRUKER_USERNAME="$(cat /var/run/secrets/nais.io/serviceuser/username)"
