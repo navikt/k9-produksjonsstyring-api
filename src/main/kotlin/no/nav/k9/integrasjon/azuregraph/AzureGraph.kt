7539386d7046bbf26f6594b2dbe129d9d9e63a0b
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-04-17 10:28:19 +0200

    Debug

--- src/main/kotlin/no/nav/k9/integrasjon/azuregraph/AzureGraph.kt
+++ src/main/kotlin/no/nav/k9/integrasjon/azuregraph/AzureGraph.kt
@@ -1,17 +1,28 @@
 //package no.nav.k9.integrasjon.azuregraph
 //
 //import com.github.kittinunf.fuel.core.Headers
+//import com.github.kittinunf.fuel.httpPost
 //import com.microsoft.graph.requests.extensions.GraphServiceClient
-//import com.microsoft.graph.requests.extensions.IDirectoryObjectCollectionPage
-//import io.ktor.html.each
+//import io.ktor.http.HttpHeaders
 //import io.ktor.http.Url
 //import no.nav.helse.dusseldorf.ktor.client.buildURL
 //import no.nav.helse.dusseldorf.oauth2.client.AccessTokenClient
 //import no.nav.helse.dusseldorf.oauth2.client.CachedAccessTokenClient
+//import no.nav.k9.aksjonspunktbehandling.objectMapper
 //import no.nav.k9.domene.oppslag.Ident
+//import no.nav.k9.integrasjon.rest.NavHeaders
+//import no.nav.k9.integrasjon.rest.idToken
 //import org.slf4j.Logger
 //import org.slf4j.LoggerFactory
+//import org.springframework.http.HttpEntity
+//import org.springframework.http.HttpMethod
+//import org.springframework.http.ResponseEntity
+//import org.springframework.util.MultiValueMap
+//import org.springframework.web.client.RestClientException
+//import org.springframework.web.client.RestTemplate
 //import java.net.URI
+//import java.util.*
+//import kotlin.coroutines.coroutineContext
 //
 //
 //class AzureGraph(
@@ -29,7 +40,7 @@
 //        baseUrl = URI("https://graph.microsoft.com/v1.0/me/memberOf"),
 //        pathParts = listOf()
 //    ).toString()
-//    
+//
 //    internal suspend fun person(ident: Ident): String {
 //        val graphClient = GraphServiceClient.builder().authenticationProvider { request ->
 //            request.addHeader(
@@ -37,16 +48,26 @@
 //                "Bearer " + cachedAccessTokenClient.getAccessToken(scope)
 //            )
 //        }.buildClient()
-//
-//        val memberOf: IDirectoryObjectCollectionPage = graphClient.me().memberOf()
-//            .buildRequest()
-//            .get()
-//        memberOf.each{placeholderItem -> placeholderItem. }
-//
-//        return json
+//        return ""
 //    }
+//    fun hentAccessToken(): AadAccessToken? {
 //
-//  
+//        val httpRequest = personUrl
+//            .httpPost()
+//            .body(
+//                objectMapper().writeValueAsString(
+//                    queryRequest
+//                )
+//            )
+//            .header(
+//                "grant_type" to "client_credentials",
+//                "client_id" to "configuration.getmsid",
+//                "client_secret" to "configuration.getmsSecret",
+//                "scope" to "https://graph.microsoft.com/.default",
+//                HttpHeaders.ContentType to "application/json"
+//            )
+//        
+//    }
 //}
 //
 //
