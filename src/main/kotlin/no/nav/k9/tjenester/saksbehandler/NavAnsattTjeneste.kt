7ee48d6445d7a339df39d40dc63ee36f515fc39f
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-04-29 16:21:57 +0200

    Disabler abac

--- src/main/kotlin/no/nav/k9/tjenester/saksbehandler/NavAnsattTjeneste.kt
+++ src/main/kotlin/no/nav/k9/tjenester/saksbehandler/NavAnsattTjeneste.kt
@@ -1,15 +1,9 @@
 package no.nav.k9.tjenester.saksbehandler
 
-import no.nav.k9.tilgangskontroll.Policies
 import no.nav.k9.tilgangskontroll.Tilgangskontroll
-import no.nav.k9.tilgangskontroll.rsbac.DecisionEnums
 
 open class NavAnsattTjeneste(
     private val tilgangskontroll: Tilgangskontroll
 ) {
-
-    fun saksbehandlerHarTilgangTilDiskresjonskode(diskresjonskode: String): Boolean {
-        return tilgangskontroll.check(Policies.tilgangTilKode6.with(diskresjonskode))
-            .getDecision().decision == DecisionEnums.PERMIT
-    }
+    
 }
