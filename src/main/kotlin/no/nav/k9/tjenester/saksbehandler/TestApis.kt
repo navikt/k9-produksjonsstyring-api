853ca62d86ccffdabefaac88f8c282e2ed4b065e
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-05-02 14:31:49 +0200

    Tester abac med ident

--- src/main/kotlin/no/nav/k9/tjenester/saksbehandler/TestApis.kt
+++ src/main/kotlin/no/nav/k9/tjenester/saksbehandler/TestApis.kt
@@ -66,8 +66,8 @@ internal fun Route.TestApis(
                 idToken = call.idToken()
             )
         ) {
-            val erOppgaveStyrer = pepClient.erOppgaveStyrer(call.idToken())
-            val harbasistilgang = true //pepClient.harBasisTilgang(call.idToken())
+            val erOppgaveStyrer = true // pepClient.erOppgaveStyrer(call.idToken())
+            val harbasistilgang = pepClient.harBasisTilgang(call.idToken())
 
 
 
