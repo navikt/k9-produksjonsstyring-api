9742667410eb1c3e24b5c2a2eaad34f17b5e7c2a
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-05-12 15:51:38 +0200

    La til behandlingstype

--- src/main/kotlin/no/nav/k9/aksjonspunktbehandling/K9sakEventHandler.kt
+++ src/main/kotlin/no/nav/k9/aksjonspunktbehandling/K9sakEventHandler.kt
@@ -2,6 +2,7 @@ package no.nav.k9.aksjonspunktbehandling
 
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.k9.Configuration
+import no.nav.k9.domene.modell.BehandlingType
 import no.nav.k9.domene.modell.Modell
 import no.nav.k9.domene.repository.BehandlingProsessEventRepository
 import no.nav.k9.domene.repository.OppgaveKøRepository
@@ -73,11 +74,11 @@ class K9sakEventHandler @KtorExperimentalAPI constructor(
         val sisteEvent = modell.sisteEvent()
         val behandlingOpprettet = BehandlingOpprettet(
             hendelseType = "behandlingOpprettet",
-            hendelsesId = """${sisteEvent.eksternId.toString()}_${modell.eventer.size}""",
+            hendelsesId = sisteEvent.eksternId.toString() +"_"+ modell.eventer.size,
             hendelsesprodusentREF = BehandlingOpprettet.HendelsesprodusentREF("", "", "K9"),
             hendelsesTidspunkt = sisteEvent.eventTid,
             behandlingsID = ("k9-los-" + sisteEvent.behandlingId),
-            behandlingstype = BehandlingOpprettet.Behandlingstype("", "", sisteEvent.behandlingTypeKode),
+            behandlingstype = BehandlingOpprettet.Behandlingstype("", "", BehandlingType.fraKode(sisteEvent.behandlingTypeKode).kodeverk),
             sakstema = BehandlingOpprettet.Sakstema("", "", "OMS"),
             behandlingstema = BehandlingOpprettet.Behandlingstema(
                 "ab0149",
