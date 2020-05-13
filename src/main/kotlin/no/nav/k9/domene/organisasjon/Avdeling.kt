f9e7023478603ecdfa38c4fa290706b2b4c64096
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-05-13 14:31:11 +0200

    Tok bort ubrukt kode

--- src/main/kotlin/no/nav/k9/domene/organisasjon/Avdeling.kt
+++ src/main/kotlin/no/nav/k9/domene/organisasjon/Avdeling.kt
@@ -1,7 +1,6 @@
 package no.nav.k9.domene.organisasjon
 
 import no.nav.k9.domene.modell.OppgaveKø
-import java.util.Collections
 
 data class Avdeling(
     val id: Long,
@@ -9,14 +8,4 @@ data class Avdeling(
     val navn: String,
     private val saksbehandlere: List<Saksbehandler>,
     private val oppgaveKø: List<OppgaveKø>
-) {
-    val kreverKode6: Boolean? = java.lang.Boolean.FALSE
-    fun getSaksbehandlere(): List<Saksbehandler> {
-        return Collections.unmodifiableList(saksbehandlere!!)
-    }
-    fun getOppgaveFiltrering(): List<OppgaveKø> {
-        return Collections.unmodifiableList(oppgaveKø!!)
-    }
-    val AVDELING_DRAMMEN_ENHET = "4806"
-
-}
+) {}
