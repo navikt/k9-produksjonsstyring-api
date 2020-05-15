5bc48bbf3c92f23d7ceca221fa992352a858a259
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-05-15 13:15:21 +0200

    Unknown blob

--- src/main/kotlin/no/nav/k9/domene/lager/oppgave/Reservasjon.kt
+++ src/main/kotlin/no/nav/k9/domene/lager/oppgave/Reservasjon.kt
@@ -14,7 +14,6 @@ data class Reservasjon(
     val oppgave: UUID
 ) {
     fun erAktiv(reservasjonRepository: ReservasjonRepository): Boolean {
-        
         return if (aktiv && reservertTil!!.isAfter(LocalDateTime.now())) {
             true
         } else {
