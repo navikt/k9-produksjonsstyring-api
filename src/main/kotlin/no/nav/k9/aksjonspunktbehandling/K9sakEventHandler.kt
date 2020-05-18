5caef395b4557ea18b411941ab93454b49a0ebbe
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-05-18 11:40:37 +0200

    Feil i statistikk topic

--- src/main/kotlin/no/nav/k9/aksjonspunktbehandling/K9sakEventHandler.kt
+++ src/main/kotlin/no/nav/k9/aksjonspunktbehandling/K9sakEventHandler.kt
@@ -54,7 +54,7 @@ class K9sakEventHandler @KtorExperimentalAPI constructor(
                 behandlingAvsluttet(modell, sakOgBehadlingProducer)
             }
 
-            if (config.erIDevFss) {
+            if (false &&config.erIDevFss) {
                 statistikkProducer.sendSak(modell.dvhSak())
                 statistikkProducer.sendBehandling(
                     modell.dvhBehandling(
