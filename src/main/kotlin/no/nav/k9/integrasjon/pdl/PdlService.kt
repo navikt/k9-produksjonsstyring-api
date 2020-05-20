1a37bbf6f41a1b295e09d45dd06ecd445d9a8a58
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-05-20 14:56:44 +0200

    Kutter q2 ident på fnummer

--- src/main/kotlin/no/nav/k9/integrasjon/pdl/PdlService.kt
+++ src/main/kotlin/no/nav/k9/integrasjon/pdl/PdlService.kt
@@ -202,7 +202,7 @@ class PdlService @KtorExperimentalAPI constructor(
         val queryRequest = QueryRequest(
             getStringFromResource("/pdl/hentIdent.graphql"),
             mapOf(
-                "ident" to getQ2Ident(fnummer, configuration = configuration),
+                "ident" to fnummer, configuration = configuration,
                 "historikk" to "false",
                 "grupper" to listOf("AKTORID")
             )
