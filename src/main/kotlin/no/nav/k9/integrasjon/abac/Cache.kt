4b9944129c2895f67f4a31bb3c8fcf5dc0009f16
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-06-12 12:34:23 +0200

    Caching av pep

---
+++ src/main/kotlin/no/nav/k9/integrasjon/abac/Cache.kt
@@ -0,0 +1,35 @@
+package no.nav.k9.integrasjon.abac
+
+import java.time.LocalDateTime
+
+class Cache {
+    private val map =
+        object : LinkedHashMap<String, CacheObject>(initialCapacity = 1000, loadFactor = .75f, accessOrder = true) {
+            override fun removeEldestEntry(eldest: MutableMap.MutableEntry<String, CacheObject>): Boolean {
+                val tooManyCachedItems = size > 1000
+                if (tooManyCachedItems) {
+                    this.remove(eldest.key)
+                }
+                return tooManyCachedItems
+            }
+        }
+
+    fun set(key: String, value: CacheObject) {
+        map[key] = value
+    }
+
+    fun remove(key: String) = map.remove(key)
+
+    fun get(key: String): CacheObject? {
+
+        val cacheObject = map[key] ?: return null
+        if (cacheObject.expire.isBefore(LocalDateTime.now())) {
+            return null
+        }
+        return cacheObject
+    }
+
+    fun clear() {
+        map.clear()
+    }
+}
