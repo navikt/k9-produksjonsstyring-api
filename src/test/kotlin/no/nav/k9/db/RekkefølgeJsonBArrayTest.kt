84ec4caa045697eb86cb66582fc536dfa9e00b2b
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-06-15 07:33:29 +0200

    Tester rekkefølge i jsonb objecter

--- src/test/kotlin/no/nav/k9/db/RekkefølgeJsonBArrayTest.kt
+++ src/test/kotlin/no/nav/k9/db/RekkefølgeJsonBArrayTest.kt
@@ -2,22 +2,12 @@ package no.nav.k9.db
 
 import com.opentable.db.postgres.embedded.EmbeddedPostgres
 import io.ktor.util.KtorExperimentalAPI
-import io.mockk.mockk
-import kotlinx.coroutines.channels.Channel
-import no.nav.k9.Configuration
 import no.nav.k9.domene.lager.oppgave.Oppgave
 import no.nav.k9.domene.modell.Aksjonspunkter
 import no.nav.k9.domene.modell.BehandlingStatus
 import no.nav.k9.domene.modell.BehandlingType
 import no.nav.k9.domene.modell.FagsakYtelseType
-import no.nav.k9.domene.repository.OppgaveKøRepository
 import no.nav.k9.domene.repository.OppgaveRepository
-import no.nav.k9.domene.repository.ReservasjonRepository
-import no.nav.k9.domene.repository.SaksbehandlerRepository
-import no.nav.k9.integrasjon.abac.PepClient
-import no.nav.k9.integrasjon.azuregraph.AzureGraphService
-import no.nav.k9.integrasjon.pdl.PdlService
-import no.nav.k9.tjenester.saksbehandler.oppgave.OppgaveTjeneste
 import org.junit.Test
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -30,32 +20,9 @@ class RekkefølgeJsonBArrayTest {
         val pg = EmbeddedPostgres.start()
         val dataSource = pg.postgresDatabase
         runMigration(dataSource)
-        val oppgaveKøOppdatert = Channel<UUID>(1)
-
+     
         val oppgaveRepository = OppgaveRepository(dataSource = dataSource)
-        val oppgaveKøRepository = OppgaveKøRepository(
-            dataSource = dataSource,
-            oppgaveKøOppdatert = oppgaveKøOppdatert,
-            oppgaveRepository = oppgaveRepository
-        )
-        val reservasjonRepository = ReservasjonRepository(
-            dataSource = dataSource,
-            oppgaveRepository = oppgaveRepository,
-            oppgaveKøRepository = oppgaveKøRepository
-        )
-        val config = mockk<Configuration>()
-        val pdlService = mockk<PdlService>()
-        val saksbehandlerRepository = SaksbehandlerRepository(dataSource = dataSource)
-        val pepClient = mockk<PepClient>()
-        val azureGraphService = mockk<AzureGraphService>()
-        val oppgaveTjeneste = OppgaveTjeneste(
-            oppgaveRepository,
-            oppgaveKøRepository,
-            saksbehandlerRepository,
-            pdlService,
-            reservasjonRepository, config, azureGraphService, pepClient
-        )
-
+          
         val eksternId = UUID.randomUUID()
         IntRange(1, 10).forEach {
             lagreOppgave(eksternId, oppgaveRepository, it)
