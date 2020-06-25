2f0038363e8587423f7217a823e053edfb65c94e
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-06-25 08:44:15 +0200

    Endret på dto for ferdigstilte

--- src/main/resources/migreringer/V1.0_009.sql
+++ src/main/resources/migreringer/V1.0_009.sql
@@ -1,8 +1,9 @@
 drop table if exists FERDIGSTILTE_BEHANDLINGER;
 create table if not exists FERDIGSTILTE_BEHANDLINGER
 (
-    behandlingType VARCHAR(100) NOT NULL PRIMARY KEY,
+    behandlingType VARCHAR(100) NOT NULL,
     dato           DATE         NOT NULL,
     data           jsonb,
+    primary key (behandlingType, dato),
     unique (behandlingType, dato)
 );
