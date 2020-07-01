5c28324cf68c679381af32515aed22f8303a263f
alexandraban <alexandrabandarenka@nav.no>
2020-07-01 14:16:37 +0200

    legger på logging

--- src/main/kotlin/no/nav/k9/aksjonspunktbehandling/K9sakEventHandler.kt
+++ src/main/kotlin/no/nav/k9/aksjonspunktbehandling/K9sakEventHandler.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.runBlocking
 import no.nav.k9.Configuration
 import no.nav.k9.domene.lager.oppgave.Oppgave
 import no.nav.k9.domene.modell.BehandlingStatus
-import no.nav.k9.domene.modell.OppgaveKø
 import no.nav.k9.domene.repository.*
 import no.nav.k9.integrasjon.datavarehus.StatistikkProducer
 import no.nav.k9.integrasjon.kafka.dto.BehandlingProsessEventDto
@@ -52,6 +51,7 @@ class K9sakEventHandler @KtorExperimentalAPI constructor(
                     statistikkRepository.lagreFerdigstilt(oppgave.behandlingType.kode, oppgave.eksternId)
                     oppgaveKøRepository.hent().forEach {kø ->
                         if (kø.oppgaverOgDatoer.map { it.id }.contains(oppgave.eksternId)) {
+                            log.info("Legger ferdigstilt oppgave " + oppgave.eksternId.toString() + " til køen " + kø.navn)
                             kø.nyeOgFerdigstilteOppgaverDto(oppgave).leggTilFerdigstilt(oppgave.eksternId.toString())
                         }
                     }
