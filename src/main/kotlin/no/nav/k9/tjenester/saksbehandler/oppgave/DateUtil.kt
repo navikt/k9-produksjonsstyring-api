3e4da5c4f07ee5b61c276be967336f7821dcf727
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-07-27 14:47:18 +0200

    Ikke mulig å reservere saker i helgen

--- src/main/kotlin/no/nav/k9/tjenester/saksbehandler/oppgave/DateUtil.kt
+++ src/main/kotlin/no/nav/k9/tjenester/saksbehandler/oppgave/DateUtil.kt
@@ -1,18 +1,13 @@
 package no.nav.k9.tjenester.saksbehandler.oppgave
 
 import java.time.DayOfWeek
-import java.time.LocalDate
 import java.time.LocalDateTime
 
 fun LocalDateTime.forskyvReservasjonsDato(): LocalDateTime {
     var localDate = this.toLocalDate()
-    var dagerSomSkalLeggesTil = 0L
-    while (localDate != LocalDate.now()) {
-        if (this.dayOfWeek == DayOfWeek.SATURDAY || this.dayOfWeek == DayOfWeek.SUNDAY) {
-            dagerSomSkalLeggesTil += 1L
-        }
-        localDate = localDate.minusDays(1)
+    while (localDate.dayOfWeek == DayOfWeek.SATURDAY || localDate.dayOfWeek == DayOfWeek.SUNDAY) {       
+        localDate = localDate.plusDays(1)
     }
 
-    return this.plusDays(dagerSomSkalLeggesTil)
+    return localDate.atStartOfDay()
 }
