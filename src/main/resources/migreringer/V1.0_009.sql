99d697eb70f73784ccbda0b23c72b3f834b81807
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-07-27 13:51:03 +0200

    Manglende migreringer

--- src/main/resources/migreringer/V1.0_009.sql
+++ src/main/resources/migreringer/V1.0_009.sql
@@ -1,9 +1,9 @@
 drop table if exists FERDIGSTILTE_BEHANDLINGER;
 create table if not exists FERDIGSTILTE_BEHANDLINGER
 (
-  behandlingType VARCHAR(100) NOT NULL,
-  dato           DATE         NOT NULL,
-  data           jsonb,
-  primary key (behandlingType, dato),
-  unique (behandlingType, dato)
+    behandlingType VARCHAR(100) NOT NULL,
+    dato           DATE         NOT NULL,
+    data           jsonb,
+    primary key (behandlingType, dato),
+    unique (behandlingType, dato)
 );
