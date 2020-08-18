1b5e6d1305c1c3647cf7d98878d00784948b5c07
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-08-18 15:24:35 +0200

    Oppdatert api

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "1.3.2.daa1b24"
+val dusseldorfKtorVersion = "1.3.2.12404e4"
 val ktorVersion = "1.3.2"
 val pdfBoxVersion = "2.0.16"
 val mainClass = "no.nav.k9.K9LosKt"
