eb0cee319f104a7491e753483e049bd5bfd8661c
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-08-24 14:30:03 +0200

    Fjernet refreshing av reserverte når de har løpt ut

--- src/main/kotlin/no/nav/k9/domene/repository/ReservasjonRepository.kt
+++ src/main/kotlin/no/nav/k9/domene/repository/ReservasjonRepository.kt
@@ -8,6 +8,7 @@ import kotliquery.sessionOf
 import kotliquery.using
 import no.nav.k9.aksjonspunktbehandling.objectMapper
 import no.nav.k9.domene.lager.oppgave.Reservasjon
+import no.nav.k9.tjenester.sse.Melding
 import no.nav.k9.tjenester.sse.SseEvent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -171,7 +172,7 @@ class ReservasjonRepository(
                     ).asUpdate
                 )
                 if (refresh && forrigeReservasjon != json) {
-            //        runBlocking { refreshKlienter.send((SseEvent(objectMapper().writeValueAsString(Melding("oppdaterReserverte"))))) }
+                   runBlocking { refreshKlienter.send((SseEvent(objectMapper().writeValueAsString(Melding("oppdaterReserverte"))))) }
                 }
             }
         }
