31ee040afb11e38cb4a6ca8319e07157ef93ddd2
alexandraban <60691224+alexandraban@users.noreply.github.com>
2020-08-26 10:31:13 +0200

    fikser flere resultater for flere saksid (#93)
    * fikser flere resultater for flere saksid
    
    * revert
    
    Co-authored-by: alexandraban <alexandrabandarenka@nav.no>

--- src/main/kotlin/no/nav/k9/tjenester/avdelingsleder/oppgaveko/YtelsesTypeDto.kt
+++ src/main/kotlin/no/nav/k9/tjenester/avdelingsleder/oppgaveko/YtelsesTypeDto.kt
@@ -1,8 +1,6 @@
 package no.nav.k9.tjenester.avdelingsleder.oppgaveko
 
-import no.nav.k9.domene.modell.FagsakYtelseType
-
 data class YtelsesTypeDto(
     val id: String,
-    val fagsakYtelseType: FagsakYtelseType?
+    val fagsakYtelseType: String?
 )
