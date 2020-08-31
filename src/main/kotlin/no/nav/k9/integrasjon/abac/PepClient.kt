ef81367d63ac5dd07b70e63fe3fff42f12db6d57
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-08-31 09:17:26 +0200

    Enable abac

--- src/main/kotlin/no/nav/k9/integrasjon/abac/PepClient.kt
+++ src/main/kotlin/no/nav/k9/integrasjon/abac/PepClient.kt
@@ -145,16 +145,15 @@ class PepClient @KtorExperimentalAPI constructor(
 
     @KtorExperimentalAPI
     override suspend fun harTilgangTilKode6(): Boolean {
-        return azureGraphService.hentIdentTilInnloggetBruker() == "Z994034" || azureGraphService.hentIdentTilInnloggetBruker() == "Z994168"
-//        val requestBuilder = XacmlRequestBuilder()
-//            .addResourceAttribute(RESOURCE_DOMENE, DOMENE)
-//            .addResourceAttribute(RESOURCE_TYPE, OPPGAVESTYRER_SKJERMET)
-//            .addAccessSubjectAttribute(SUBJECT_TYPE, INTERNBRUKER)
-//            .addAccessSubjectAttribute(SUBJECTID, azureGraphService.hentIdentTilInnloggetBruker())
-//            .addEnvironmentAttribute(ENVIRONMENT_PEP_ID, "srvk9los")
-//
-//        val decision = evaluate(requestBuilder)
-//        return decision
+        val requestBuilder = XacmlRequestBuilder()
+            .addResourceAttribute(RESOURCE_DOMENE, DOMENE)
+            .addResourceAttribute(RESOURCE_TYPE, OPPGAVESTYRER_SKJERMET)
+            .addAccessSubjectAttribute(SUBJECT_TYPE, INTERNBRUKER)
+            .addAccessSubjectAttribute(SUBJECTID, azureGraphService.hentIdentTilInnloggetBruker())
+            .addEnvironmentAttribute(ENVIRONMENT_PEP_ID, "srvk9los")
+
+        val decision = evaluate(requestBuilder)
+        return decision
     }
     
     @KtorExperimentalAPI
@@ -175,16 +174,15 @@ class PepClient @KtorExperimentalAPI constructor(
 
     @KtorExperimentalAPI
     override suspend fun erSakKode6(fagsakNummer: String): Boolean {
-       return false
-//        val requestBuilder = XacmlRequestBuilder()
-//            .addResourceAttribute(RESOURCE_DOMENE, DOMENE)
-//            .addResourceAttribute(RESOURCE_TYPE, TILGANG_SAK_KODE6)
-//            .addAccessSubjectAttribute(SUBJECT_TYPE, NONE)
-//            .addAccessSubjectAttribute(SUBJECTID, NONE)
-//            .addEnvironmentAttribute(ENVIRONMENT_PEP_ID, "srvk9los")
-//            .addResourceAttribute(RESOURCE_SAKSNR, fagsakNummer)
-//
-//        return evaluate(requestBuilder)
+        val requestBuilder = XacmlRequestBuilder()
+            .addResourceAttribute(RESOURCE_DOMENE, DOMENE)
+            .addResourceAttribute(RESOURCE_TYPE, TILGANG_SAK_KODE6)
+            .addAccessSubjectAttribute(SUBJECT_TYPE, NONE)
+            .addAccessSubjectAttribute(SUBJECTID, NONE)
+            .addEnvironmentAttribute(ENVIRONMENT_PEP_ID, "srvk9los")
+            .addResourceAttribute(RESOURCE_SAKSNR, fagsakNummer)
+
+        return evaluate(requestBuilder)
     }
 
     @KtorExperimentalAPI
