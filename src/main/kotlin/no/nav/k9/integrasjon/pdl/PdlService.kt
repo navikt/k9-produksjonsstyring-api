0b034d74e55700ce895110bb7b7439d0c1727ba6
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-09-21 09:00:59 +0200

    Logger

--- src/main/kotlin/no/nav/k9/integrasjon/pdl/PdlService.kt
+++ src/main/kotlin/no/nav/k9/integrasjon/pdl/PdlService.kt
@@ -96,7 +96,7 @@ class PdlService @KtorExperimentalAPI constructor(
             } catch (e: Exception) {
                 try {
                     val value = objectMapper().readValue<Error>(json!!)
-                    log.warn(objectMapper().writeValueAsString(value) + "aktorId callId: " + callId + " " + coroutineContext.idToken().getUsername())
+                    log.warn(objectMapper().writeValueAsString(value) + " aktorId callId: " + callId + " " + coroutineContext.idToken().getUsername())
                     if (value.errors.any { it.extensions.code == "unauthorized" }){
                         return PersonPdlResponse(true, null)
                     }
@@ -171,7 +171,7 @@ class PdlService @KtorExperimentalAPI constructor(
             } catch (e: Exception) {
                 try {
                     val value = objectMapper().readValue<Error>(json!!)
-                    log.warn(objectMapper().writeValueAsString(value) + "identifikator callId: " + callId + " " + coroutineContext.idToken().getUsername())
+                    log.warn(objectMapper().writeValueAsString(value) + " identifikator callId: " + callId + " " + coroutineContext.idToken().getUsername())
                     if (value.errors.any { it.extensions.code == "unauthorized" }){
                         return PdlResponse(true, null)
                     }
