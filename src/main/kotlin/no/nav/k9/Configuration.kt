a78c137287aad13c46b43bc1021fb1c5e34a339b
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-10-29 10:26:19 +0100

    Endrer statistikk

--- src/main/kotlin/no/nav/k9/Configuration.kt
+++ src/main/kotlin/no/nav/k9/Configuration.kt
@@ -46,7 +46,7 @@ data class Configuration(private val config: ApplicationConfig) {
 
     internal fun getAksjonspunkthendelsePunsjTopic(): String {
         return config.getOptionalString("nav.kafka.punsjAksjonshendelseTopic", secret = false)
-            ?: "private-k9punsj-aksjonspunkthendelse-v1"
+            ?: "privat-k9punsj-aksjonspunkthendelse-v1"
     }
     
     internal fun getAksjonspunkthendelseTilbakeTopic(): String {
