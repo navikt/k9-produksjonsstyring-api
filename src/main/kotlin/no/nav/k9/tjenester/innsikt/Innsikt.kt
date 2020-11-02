a93dee2e7b2ee59cb64bf2ba46f85b3326086da0
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-11-02 08:35:43 +0100

    Mer innsikt i oppgavene

--- src/main/kotlin/no/nav/k9/tjenester/innsikt/Innsikt.kt
+++ src/main/kotlin/no/nav/k9/tjenester/innsikt/Innsikt.kt
@@ -54,12 +54,7 @@ fun Route.innsiktGrensesnitt() {
                     val aksjonspunkter = oppgaveRepository.hentAktiveOppgaversAksjonspunktliste()
                     val oppgaverTotaltAktive = oppgaveRepository.hentAktiveOppgaverTotaltIkkeSkjermede()
                     p {
-                        +"Det er nå ${aksjonspunkter.sumBy { it.antall }} " +
-                                "åpne aksjonspunkter fordelt på $oppgaverTotaltAktive oppgaver, " +
-                                "$inaktiveIkkeAvsluttedeOppgaver inaktive med annen status enn avsluttet " +
-                                "(fatter vedtak ${fatterVedtakAvsluttet}, iverksetter vedtak ${iverksetterVedtakAvsluttet}, opprettet ${opprettetAvsluttet}, utredes ${utredesAvsluttet} ) " +
-                                "og $avsluttede med status avsluttet, " +
-                                "$automatiskProsesserteTotalt er prosessert automatisk"
+                        +"Det er nå ${aksjonspunkter.sumBy { it.antall }} åpne aksjonspunkter fordelt på $oppgaverTotaltAktive oppgaver, $inaktiveIkkeAvsluttedeOppgaver inaktive med annen status enn avsluttet (fatter vedtak ${fatterVedtakAvsluttet}, iverksetter vedtak ${iverksetterVedtakAvsluttet}, opprettet ${opprettetAvsluttet}, utredes ${utredesAvsluttet}) og $avsluttede med status avsluttet, $automatiskProsesserteTotalt er prosessert automatisk"
                     }
                     p {
                         +"Totalt ${oppgaverTotaltAktive + inaktiveIkkeAvsluttedeOppgaver + avsluttede}"
