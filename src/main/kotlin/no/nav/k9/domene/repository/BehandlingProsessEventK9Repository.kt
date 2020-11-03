837a4828d48d73458402a530b7d1230c034e332b
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-11-03 08:59:53 +0100

    Ny lagring av eventer

--- src/main/kotlin/no/nav/k9/domene/repository/BehandlingProsessEventK9Repository.kt
+++ src/main/kotlin/no/nav/k9/domene/repository/BehandlingProsessEventK9Repository.kt
@@ -36,6 +36,45 @@ class BehandlingProsessEventK9Repository(private val dataSource: DataSource) {
         return K9SakModell(  modell.eventer.sortedBy { it.eventTid })
     }
 
+    fun lagre(uuid: UUID, f: (K9SakModell?) -> K9SakModell): K9SakModell {
+        Databasekall.map.computeIfAbsent(object {}.javaClass.name + object {}.javaClass.enclosingMethod.name) { LongAdder() }
+            .increment()
+        var sortertModell = K9SakModell(emptyList()) 
+        using(sessionOf(dataSource)) {
+            it.transaction { tx ->
+                val run = tx.run(
+                    queryOf(
+                        "select data from behandling_prosess_events_k9 where id = :id for update",
+                        mapOf("id" to uuid.toString())
+                    )
+                        .map { row ->
+                            row.string("data")
+                        }.asSingle
+                )
+
+                val modell = if (!run.isNullOrEmpty()) {
+                    val modell = objectMapper().readValue(run, K9SakModell::class.java)
+                    f(modell.copy(eventer = modell.eventer.sortedBy { it.eventTid }))
+                } else {
+                    f(null)
+                }
+                sortertModell = modell.copy(eventer = (modell.eventer.toSet().toList().sortedBy { it.eventTid }))
+                val json = objectMapper().writeValueAsString(sortertModell)
+                tx.run(
+                    queryOf(
+                        """
+                    insert into behandling_prosess_events_k9 as k (id, data)
+                    values (:id, :data :: jsonb)
+                    on conflict (id) do update
+                    set data = :data :: jsonb
+                 """, mapOf("id" to uuid.toString(), "data" to json)
+                    ).asUpdate
+                )
+            }
+        }
+        return sortertModell
+    }
+    
     fun lagre(
         event: BehandlingProsessEventDto
     ): K9SakModell {
