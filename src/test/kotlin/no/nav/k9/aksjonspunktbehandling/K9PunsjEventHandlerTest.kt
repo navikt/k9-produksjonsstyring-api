5d9d790cc5c29f154d8385bf7d9c44f944d3c017
alexandraban <alexandrabandarenka@nav.no>
2020-11-09 12:37:02 +0100

    test

--- src/test/kotlin/no/nav/k9/aksjonspunktbehandling/K9PunsjEventHandlerTest.kt
+++ src/test/kotlin/no/nav/k9/aksjonspunktbehandling/K9PunsjEventHandlerTest.kt
@@ -1,17 +1,11 @@
 package no.nav.k9.aksjonspunktbehandling
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.util.*
 import no.nav.helse.dusseldorf.ktor.jackson.dusseldorfConfigured
 import no.nav.k9.buildAndTestConfig
-import no.nav.k9.domene.modell.Aksjonspunkter
 import no.nav.k9.domene.repository.OppgaveRepository
-import no.nav.k9.integrasjon.kafka.dto.BehandlingProsessEventDto
 import no.nav.k9.integrasjon.kafka.dto.PunsjEventDto
-import no.nav.k9.integrasjon.kafka.dto.PunsjId
-import no.nav.k9.sak.typer.AktørId
-import no.nav.k9.sak.typer.JournalpostId
 import org.intellij.lang.annotations.Language
 import org.junit.Rule
 import org.junit.Test
@@ -19,7 +13,6 @@ import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.get
 import org.slf4j.LoggerFactory
-import java.time.LocalDateTime
 import java.util.*
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
@@ -50,13 +43,6 @@ class K9PunsjEventHandlerTest : KoinTest {
                 "aksjonspunkter":{"liste":{"OPPR":"Opprettet"}}}
             """.trimIndent()
 
-        val punsjOppgave = PunsjEventDto(
-            UUID.randomUUID(),
-            JournalpostId("585886"),
-            LocalDateTime.now(),
-            AktørId("980458930"),
-            Aksjonspunkter(mapOf("OPPR" to "Opprettet"))
-        )
         val objectMapper = jacksonObjectMapper().dusseldorfConfigured()
         val event = objectMapper.readValue(json, PunsjEventDto::class.java)
 
@@ -64,5 +50,29 @@ class K9PunsjEventHandlerTest : KoinTest {
         val oppgaveModell = oppgaveRepository.hent(UUID.fromString(event.eksternId.toString()))
         val oppgave = oppgaveModell
         assertTrue { oppgave.aktiv }
+    }
+
+    @KtorExperimentalAPI
+    @Test
+    fun `Skal avslutte oppgave dersom oppgaven ikke har noen akrive aksjonspunkter`() {
+
+        val k9PunsjEventHandler = get<K9punsjEventHandler>()
+        val oppgaveRepository = get<OppgaveRepository>()
+
+        @Language("JSON") val json =
+            """{"eksternId":"871da8c5-a1a8-4353-a10b-10f6e4521490",
+                "journalpostId":"585886",
+                "eventTid":"2020-11-09T12:24:57.46806",
+                "aktørId":"980458930",
+                "aksjonspunkter":{"liste":{"AVSLU":"Avsluttet"}}}
+            """.trimIndent()
+
+        val objectMapper = jacksonObjectMapper().dusseldorfConfigured()
+        val event = objectMapper.readValue(json, PunsjEventDto::class.java)
+
+        k9PunsjEventHandler.prosesser(event)
+        val oppgaveModell = oppgaveRepository.hent(UUID.fromString(event.eksternId.toString()))
+        val oppgave = oppgaveModell
+        assertFalse { oppgave.aktiv }
     }
 }
