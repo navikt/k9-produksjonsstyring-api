d96d1911f7e5977b2a50d92afd3b7087e3176202
alexandraban <alexandrabandarenka@nav.no>
2020-12-11 09:48:52 +0100

    bruker en ny token

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,7 +95,7 @@ repositories {
         url = uri("https://maven.pkg.github.com/navikt/dusseldorf-ktor")
         credentials {
             username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
-            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GPR_TOKEN")
         }
     }
 
