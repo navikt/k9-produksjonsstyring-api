1e2699f36acff8bc1f1d48eac266c1a7e4be3429
alexandraban <alexandrabandarenka@nav.no>
2021-01-06 14:39:34 +0100

    søk på fagsaker med aktørid

--- src/main/kotlin/no/nav/k9/tjenester/fagsak/FagsakApis.kt
+++ src/main/kotlin/no/nav/k9/tjenester/fagsak/FagsakApis.kt
@@ -11,11 +11,18 @@ import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.withContext
 import no.nav.k9.Configuration
 import no.nav.k9.KoinProfile
+import no.nav.k9.domene.modell.BehandlingStatus
+import no.nav.k9.domene.modell.BehandlingType
+import no.nav.k9.domene.modell.FagsakYtelseType
 import no.nav.k9.integrasjon.rest.IRequestContextService
 import no.nav.k9.tjenester.saksbehandler.idToken
+import no.nav.k9.tjenester.saksbehandler.oppgave.OppgaveDto
+import no.nav.k9.tjenester.saksbehandler.oppgave.OppgaveStatusDto
 import no.nav.k9.tjenester.saksbehandler.oppgave.OppgaveTjeneste
 import no.nav.k9.tjenester.saksbehandler.oppgave.SokeResultatDto
 import org.koin.ktor.ext.inject
+import java.time.LocalDateTime
+import java.util.*
 
 @KtorExperimentalAPI
 @KtorExperimentalLocationsAPI
@@ -23,6 +30,7 @@ internal fun Route.FagsakApis() {
     val oppgaveTjeneste by inject<OppgaveTjeneste>()
     val configuration by inject<Configuration>()
     val requestContextService by inject<IRequestContextService>()
+
     @Location("/sok")
     class søkFagsaker
 
@@ -39,6 +47,57 @@ internal fun Route.FagsakApis() {
                 )
             ) {
                 call.respond(oppgaveTjeneste.søkFagsaker(søk.searchString))
+            }
+        }
+    }
+
+    @Location("/aktoerid-sok")
+    class søkFagsakerMedAktørId
+
+    post { _: søkFagsakerMedAktørId ->
+        val param = call.receive<AktoerIdDto>()
+        if (KoinProfile.LOCAL == configuration.koinProfile()) {
+            call.respond(
+                SokeResultatDto(
+                    false, PersonDto("WalterWhite", "23908978759", "MANN", null), mutableListOf(
+                        OppgaveDto(
+                            OppgaveStatusDto(
+                                false,
+                                null,
+                                false,
+                                null,
+                                null),
+                            47589675,
+                            null,
+                            "5zYs4",
+                            "Walter White",
+                            "K9SAK",
+                            "18056387079",
+                            BehandlingType.FORSTEGANGSSOKNAD,
+                            FagsakYtelseType.OMSORGSPENGER,
+                            BehandlingStatus.OPPRETTET,
+                            true,
+                            LocalDateTime.now(),
+                            LocalDateTime.now().plusDays(24),
+                            UUID.randomUUID(),
+                            false,
+                            false,
+                            false,
+                            false,
+                            false,
+                            false)
+                    )
+                )
+            )
+        } else {
+            val idToken = call.idToken()
+            withContext(
+                requestContextService.getCoroutineContext(
+                    context = coroutineContext,
+                    idToken = idToken
+                )
+            ) {
+                call.respond(oppgaveTjeneste.finnOppgaverBasertPåAktørId(param.aktoerId))
             }
         }
     }
