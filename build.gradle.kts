ac39d940ba885af4c11b4e7f752a8aa5de4ab299
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-03-11 12:11:20 +0100

    Fjerner noen repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,10 +98,7 @@ repositories {
     maven("https://packages.confluent.io/maven/")
     maven("https://jitpack.io")
 
-    // bintray og jcenter stenges i slutten av April 2021
-    maven("https://dl.bintray.com/kotlin/ktor")
-    maven("https://kotlin.bintray.com/kotlinx")
-    jcenter()
+    jcenter() // https://github.com/InsertKoinIO/koin#jcenter
 }
 
 
