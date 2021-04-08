b15a50fae06ec8ab8b79658b1204e50b1d16da7e
Ole Jørgen Rishoff <olejri@gmail.com>
2021-04-08 12:42:13 +0200

    Oppdaterer til siste versjon av kodeverk (#267)

--- src/main/kotlin/no/nav/k9/aksjonspunktbehandling/K9sakEventHandler.kt
+++ src/main/kotlin/no/nav/k9/aksjonspunktbehandling/K9sakEventHandler.kt
@@ -35,7 +35,8 @@ class K9sakEventHandler @KtorExperimentalAPI constructor(
     val tillatteYtelseTyper = listOf(
         FagsakYtelseType.OMSORGSPENGER,
         FagsakYtelseType.PLEIEPENGER_SYKT_BARN,
-        FagsakYtelseType.OMSORGSPENGER_KS
+        FagsakYtelseType.OMSORGSPENGER_KS,
+        FagsakYtelseType.OMSORGSPENGER_MA
     )
 
     @KtorExperimentalAPI
